30a0a1bd9db2a4220f147f5f862d11d94a56a04a
esteban alvarado <estalvgs1999@gmail.com>
2021-07-01 11:33:03 -0600

    feat(home): Get user ip address from request object

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello World"
+    user_ip = request.remote_addr
+    return f'Hello World! Your Ip:{user_ip}'
+
+if __name__ == '__main__':
+    app.run(debug=True)
