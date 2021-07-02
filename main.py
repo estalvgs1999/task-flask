7f6d95b3f399c22fdb326e8c9c829da196f2d595
esteban alvarado <estalvgs1999@gmail.com>
2021-07-01 20:06:45 -0600

    feat(signin): SignIn Page with styles

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField,SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '36d4J0Ltp3lRtee9HDxY3K'
@@ -7,6 +10,12 @@ app.config['SECRET_KEY'] = '36d4J0Ltp3lRtee9HDxY3K'
 bootstrap = Bootstrap(app)
 
 tasks = ['Task 1','Task 2','Task 3']
+
+class LoginForm(FlaskForm):
+    username = StringField('Username or email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign in')
+
 
 @app.errorhandler(404)
 def not_found(error):
@@ -26,12 +35,13 @@ def index():
     return response
 
 
-@app.route('/home')
+@app.route('/home', methods=['GET','POST'])
 def home():
     user_ip = session.get('user_ip')
     context = {
         'user_ip': user_ip,
-        'tasks': tasks
+        'tasks': tasks,
+        'login_form': LoginForm()
     }
     return render_template('home.html', **context)
 
