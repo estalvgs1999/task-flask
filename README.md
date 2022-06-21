8c7033256dd1a3a7e0077ce1bbd4ffaa42f632fb
Esteban Alvarado <estalvgs1999@estudiantec.cr>
2022-06-20 21:59:20 -0600

    [class-23] blueprints 1

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 To Do List type app that will allow you to create, edit and delete tasks. Developed in Platzi's Flask course.
 
 ### Preview
-![login-sample](https://raw.githubusercontent.com/estalvgs1999/task-flask/master/static/assets/sample-auth.gif)
+![login-sample](https://github.com/estalvgs1999/task-flask/blob/feature/app/app/static/assets/sample-login.gif)
--- tests/test_base.py
+++ tests/test_base.py
@@ -3,6 +3,7 @@ from flask_testing import TestCase
 from flask import current_app
 from main import app
 
+
 class MainTest(TestCase):
 
     def create_app(self):
@@ -10,25 +11,20 @@ class MainTest(TestCase):
         app.config['WTF_CSRF_ENABLED'] = False
         return app
 
-
     def test_app_exists(self):
         self.assertIsNotNone(current_app)
 
-
     def test_app_in_test_mode(self):
         self.assertTrue(current_app.config['TESTING'])
-
 
     def test_index_redirects(self):
         response = self.client.get(url_for('index'))
         self.assertRedirects(response, url_for('login'))
 
-    
     def test_login_get(self):
         response = self.client.get(url_for('login'))
         self.assert200(response)
 
-    
     def test_login_post(self):
         fake_form = {
             'username': 'esteban',
@@ -36,3 +32,14 @@ class MainTest(TestCase):
         }
         response = self.client.post(url_for('login'), data=fake_form)
         self.assertRedirects(response, url_for('home'))
+
+    def test_auth_blueprint_exists(self):
+        self.assertIn('auth', self.app.blueprints)
+
+    def test_auth_login_get(self):
+        response = self.client.get(url_for('auth.login'))
+        self.assert200(response)
+
+    def test_auth_login_template(self):
+        self.client.get(url_for('auth.login'))
+        self.assertTemplateUsed('login.html')
